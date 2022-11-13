e9f43bfe6f6a022f4721d53c9ea69f6114251b50
Magnun A V F <magnunavf@gmail.com>
2022-11-13 19:46:44 -0300

    Create logger

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+const { log } = require('./utils/log')
+
 const app = express()
 dotenv.config()
 
 const port = process.env.PORT || 3000
+const env = process.env.ENV || 'dev'
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  // console.log(`Example app listening on port ${port}`);
+  log.info(`Running in ${env} mode in http://127.0.0.1:${port}`)
 })
