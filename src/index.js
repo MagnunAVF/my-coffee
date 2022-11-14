e897423a0b859a6884bb67fee2ce064dc34d00bc
Magnun A V F <magnunavf@gmail.com>
2022-11-13 23:47:47 -0300

    Create views layout

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 const express = require('express')
+const path = require('path')
 const dotenv = require('dotenv')
+const expressLayouts = require('express-ejs-layouts')
 
 const { log } = require('./utils/log')
 
 const app = express()
 dotenv.config()
 
+// env vars
 const port = process.env.PORT || 3000
 const env = process.env.ENV || 'dev'
 
+// assets folder
+app.use(express.static(__dirname + '/public'))
+
+// template engine and layouts
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(expressLayouts)
+app.set('layout', './components/layout.ejs')
+
+// routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index', { title: 'Home Page' })
 })
 
+// start server
 app.listen(port, () => {
   log.info(`Running in ${env} mode in http://127.0.0.1:${port}`)
 })
