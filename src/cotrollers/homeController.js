5054eeb1b35df477ce37fba3dd7dcac9c1148f1a
Magnun A V F <magnunavf@gmail.com>
2022-11-15 06:46:45 -0300

    Add notifications in layout

--- src/cotrollers/homeController.js
+++ src/cotrollers/homeController.js
@@ -1,5 +1,5 @@
 const homeView = (req, res) => {
-  res.render('index', { title: 'My Coffee - Home Page' })
+  res.render('index', { title: 'My Coffee - Home Page', notification: false })
 }
 
 module.exports = {
