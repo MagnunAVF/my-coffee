9216f72f0094b77b888d1818236d50fdca6444f5
Magnun A V F <magnunavf@gmail.com>
2022-11-20 14:54:31 -0300

    Protect admin routes

--- src/auth/protect.js
+++ src/auth/protect.js
@@ -1,18 +1,28 @@
-const protectRoute = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next()
+const { renderWithError } = require('../utils/response')
+
+const ADMIN_ROUTES = ['/admin']
+
+const protectRoute = async (req, res, next) => {
+  // Check if user is authenticated
+  if (!req.isAuthenticated()) {
+    await renderWithError(req, res, 'login', 'Login', 'Login to continue')
+  } else {
+    // Check admin route
+    const route = req.route.path
+    const isAdminRoute = ADMIN_ROUTES.includes(route)
+
+    if (isAdminRoute) {
+      const userType = req.user.type
+
+      if (userType === 'admin') {
+        return next()
+      } else {
+        await renderWithError(req, res, 'index', 'Home', 'Invalid Page')
+      }
+    } else {
+      return next()
+    }
   }
-
-  const message = 'Login to continue'
-  log.warn(message)
-
-  res.render('login', {
-    title: 'My Coffee - Login',
-    notification: {
-      type: 'error',
-      message,
-    },
-  })
 }
 
 const allowIf = (req, res, next) => {
